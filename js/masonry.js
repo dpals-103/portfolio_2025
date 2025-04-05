a13b73f81a128454f4517509508bc44c69f19ab3
dpals-103 <dpals103@gmail.com>
2025-04-06 02:06:47 +0900

    v0.12 work detail page 생성성

--- js/masonry.js
+++ js/masonry.js
@@ -4,10 +4,3 @@ document.addEventListener('DOMContentedLoaded', () => {
     
 });
 
-var msnry = new Masonry('.grid', {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
-    gutter: 40,
-    fitWidth: false
-});
--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     class Header extends HTMLElement {
         connectedCallback() {
             this.innerHTML =
-            `<span class="year">2025</span>
+                `<span class="year">2025</span>
              <a href="/"><strong class="logo">©LEE HYO JU</strong></a>
             <nav>
                 <a href="about.html">About</a>
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const headerYear = document.querySelector('custom-header .year');
     const headerLogo = document.querySelector('custom-header .logo');
 
-    if(currentPath.endsWith("index.html") || currentPath === "/"){
+    if (currentPath.endsWith("index.html") || currentPath === "/") {
         headerLogo.style.display = 'none';
     } else {
         headerYear.style.display = 'none';
@@ -34,10 +34,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
     gnbLink.forEach((link) => {
         const linkPath = new URL(link.href, window.location.origin).pathname; // 절대경로 변환
-        if(linkPath === currentPath){
+        console.log(linkPath);
+        if (linkPath === currentPath) {
+            link.classList.add('active');
+        } else if (linkPath.includes('work') && currentPath.includes('work')) {
             link.classList.add('active');
         }
     });
-        
-    
+
+
+
+    const totalPages = 8; // 전체 프로젝트 개수    
+    const prevBtn = document.querySelector(".prev-btn")
+    const nextBtn = document.querySelector(".next-btn")
+    const currentPage = parseInt(currentPath.match(/work-(\d+)/)?.[1] || "1", 10);
+
+    // 이전 페이지 버튼
+    if (currentPage > 1) {
+        const prevPage = `work-0${currentPage - 1}.html`;
+        prevBtn.href = prevPage
+    } else{
+        prevBtn.style.display="none";
+    }
+
+
+    // 다음 페이지 버튼
+    if (currentPage < totalPages) {
+        const nextPage = `work-0${currentPage + 1}.html`;
+        nextBtn.href = nextPage
+    } else{
+        nextBtn.style.display="none";
+    }
+
 });
